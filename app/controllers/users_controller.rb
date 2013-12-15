9b27fd810b7152b3d563aadd00c641e60f7e25c8
amhayslip <amhayslip@gmail.com>
2013-12-15 13:49:25 -0600

    Add views for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,35 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to root_path
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path
+  end
+
+
+
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :location, :email)
+  end
+
 end
